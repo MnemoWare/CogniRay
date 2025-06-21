933b41105c026c24d8f4ddf27ada5c0009bcd73d
sombressoul <sombressoul@gmail.com>
2025-06-21 15:53:56 +0300

    Code cleanup

--- examples/ex05_minimal_hpm_memory_update_01_delta.py
+++ examples/ex05_minimal_hpm_memory_update_01_delta.py
@@ -3,8 +3,6 @@ import sys
 import torch
 import torch.nn.functional as F
 
-from typing import Union
-
 script_dir = os.path.dirname(os.path.abspath(__file__))
 project_dir = os.path.dirname(script_dir)
 sys.path.append(project_dir)

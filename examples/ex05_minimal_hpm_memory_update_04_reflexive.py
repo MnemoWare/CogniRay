a7ec25c3c7a96fc7f2d51e73ff4fe6b51e97aa16
sombressoul <sombressoul@gmail.com>
2025-06-21 15:50:45 +0300

    Update ex05 code

--- examples/ex05_minimal_hpm_memory_update_04_reflexive.py
+++ examples/ex05_minimal_hpm_memory_update_04_reflexive.py
@@ -20,14 +20,16 @@ torch.manual_seed(1337)
 #   The details and interpretation of the experiment are described in: /docs/Experiment 05.md
 #
 # Result - Stage A:
-#   Stage A: step 00 | MSE: 0.84024251
+#   Stage A: step 000 | MSE: 0.84024251
 #   ...
-#   Stage A: step 24 | MSE: 0.00002203
+#   Stage A: step 016 | MSE: 0.00074152
+#   Stage A: target loss 0.001 reached on step 016
 #
 # Result - Stage B:
-#   Stage B: step 00 | MSE: 0.93652594 | MSE A/other: 0.00001419/1.17065382
+#   Stage B: step 000 | MSE: 0.93666965 | MSE A/other: 0.00047778/1.17071760
 #   ...
-#   Stage B: step 49 | MSE: 0.00074533 | MSE A/other: 0.00001345/0.00092830
+#   Stage B: step 016 | MSE: 0.00088359 | MSE A/other: 0.00047126/0.00098667
+#   Stage B: target loss 0.001 reached on step 016
 #
 
 # Params
@@ -36,11 +38,11 @@ side = 64
 
 # Stage A params
 stage_A_alpha = 0.005
-stage_A_steps = 25
+stage_A_target_loss = 1.0e-3
 
 # Stage B params
-stage_B_alpha_reflexive = 0.001
-stage_B_steps_reflexive = 50
+stage_B_alpha = 0.001
+stage_B_target_loss = 1.0e-3
 
 # Global params
 tau = 8.0
@@ -48,21 +50,6 @@ sigma = 1.25
 eps = 1.0e-3
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-def save_memory_state(
-    memory: torch.Tensor,
-    target_A: torch.Tensor,
-    target_other: Union[torch.Tensor, None],
-    prefix: str,
-) -> None:
-    torch.save(
-        obj=dict(
-            memory = memory,
-            target_A = target_A,
-            target_other = target_other,
-        ),
-        f=f"{script_dir}/data/ex05/05_{prefix + '_' if prefix is not None else ''}memory_state.datarec.pt",
-    )
 
 # Init mem
 hpm = MinimalHPM(
@@ -81,10 +68,10 @@ ray_A_direction = F.normalize(torch.tensor([+1.0, +0.0, +0.0], device=device) + 
 ray_A_target = torch.randn([channels], device=device)
 
 # Stage A: 
-memory_dynamics_buffer = []
-
 print("Begin Stage A: imprinting target A.")
-for step in range(stage_A_steps):
+step = 0
+target_loss_reached = False
+while target_loss_reached is not True:
     with torch.no_grad():
         origins = torch.stack([ray_A_origin], dim=0)
         directions = torch.stack([ray_A_direction], dim=0)
@@ -92,15 +79,16 @@ for step in range(stage_A_steps):
 
         projections = hpm.read(origins, directions)
         loss = F.mse_loss(projections, targets.to(device))
-        print(f"Stage A: step {step:02d} | MSE: {loss.item():.8f}")
+        print(f"Stage A: step {step:03d} | MSE: {loss.item():.8f}")
 
         delta = targets - projections
         hpm.write_delta(origins, directions, delta, stage_A_alpha)
 
-        memory_dynamics_buffer.append(hpm.memory.data.clone().detach().cpu())
+        if loss.item() <= stage_A_target_loss:
+            target_loss_reached = True
+            print(f"Stage A: target loss {stage_A_target_loss} reached on step {step:03d}")
 
-memory_dynamics_buffer = torch.stack(memory_dynamics_buffer, dim=0)
-save_memory_state(memory_dynamics_buffer, ray_A_target.data, None, "stage_a_dynamics")
+    step += 1
 
 # Stage B:
 ray_other_origin = torch.tensor(
@@ -119,11 +107,10 @@ ray_other_direction = F.normalize(torch.tensor(
     ], device=device) + eps, dim=0)
 ray_other_target = torch.randn([ray_other_origin.shape[0], channels], device=device)
 
-memory_dynamics_buffer = []
-memory_dynamics_buffer.append(hpm.memory.data.clone().detach().cpu()) # Last state
-
 print("Begin Stage B: project four random targets into overlapping region. Do not update A.")
-for step in range(stage_B_steps_reflexive):
+step = 0
+target_loss_reached = False
+while target_loss_reached is not True:
     with torch.no_grad():
         origins = torch.cat([ray_A_origin.unsqueeze(0), ray_other_origin], dim=0)
         directions = torch.cat([ray_A_direction.unsqueeze(0), ray_other_direction], dim=0)
@@ -133,12 +120,13 @@ for step in range(stage_B_steps_reflexive):
         loss_A = F.mse_loss(projections[0], targets[0].to(device))
         loss_other = F.mse_loss(projections[1:], targets[1:].to(device))
         loss = F.mse_loss(projections, targets.to(device))
-        print(f"Stage B: step {step:02d} | MSE: {loss.item():.8f} | MSE A/other: {loss_A.item():.8f}/{loss_other.item():.8f}")
+        print(f"Stage B: step {step:03d} | MSE: {loss.item():.8f} | MSE A/other: {loss_A.item():.8f}/{loss_other.item():.8f}")
 
         delta = targets - projections
-        hpm.write_reflexive(origins[1:], directions[1:], delta[1:], stage_B_alpha_reflexive)
+        hpm.write_reflexive(origins[1:], directions[1:], delta[1:], stage_B_alpha)
 
-        memory_dynamics_buffer.append(hpm.memory.data.clone().detach().cpu())
+        if loss_other.item() <= stage_B_target_loss:
+            target_loss_reached = True
+            print(f"Stage B: target loss {stage_B_target_loss} reached on step {step:03d}")
 
-memory_dynamics_buffer = torch.stack(memory_dynamics_buffer, dim=0)
-save_memory_state(memory_dynamics_buffer, ray_A_target.data, ray_other_target.data, "stage_b_dynamics")
+    step += 1
